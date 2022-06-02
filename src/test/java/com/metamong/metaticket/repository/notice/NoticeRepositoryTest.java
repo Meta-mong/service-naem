1a3357eb016342cc2e55cac2486c3ec28ea87055
ss2ol <93hjs_@naver.com>
2022-06-02 16:50:03 +0900

    feat:notice update/delete

--- src/test/java/com/metamong/metaticket/repository/notice/NoticeRepositoryTest.java
+++ src/test/java/com/metamong/metaticket/repository/notice/NoticeRepositoryTest.java
@@ -1,10 +1,11 @@
 package com.metamong.metaticket.repository.notice;
 
+import com.metamong.metaticket.controller.notice.NoticeController;
 import com.metamong.metaticket.domain.admin.Admin;
 import com.metamong.metaticket.domain.notice.Notice;
 import com.metamong.metaticket.domain.notice.dto.NoticeDTO;
 import com.metamong.metaticket.repository.admin.AdminRepository;
-import com.metamong.metaticket.repository.notice.NoticeRepository;
+
 
 import com.metamong.metaticket.service.notice.NoticeService;
 import org.junit.Assert;
@@ -14,8 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
-import java.util.List;
+
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,6 +36,9 @@ class NoticeRepositoryTest {
     @Autowired
     private NoticeService noticeService;
 
+    @Autowired
+    private NoticeController noticeController;
+
     @Test
     @DisplayName("공시사항 삽입 테스트1")
     public void InsertTest(){
@@ -49,20 +55,6 @@ class NoticeRepositoryTest {
         System.out.println(temp.toString());
     }
 
-    @Test
-    @DisplayName("공지사항 수정 테스트")
-    public void updateTest() throws Exception{
-
-        Notice findNotice = noticeRepository.findById(1L).get();
-        findNotice.update("테스트1","테스트2","테스트3");
-        Notice updateNotice = noticeRepository.save(findNotice);
-
-        assertAll(
-                () -> assertEquals(updateNotice.getTitle(),"테스트2"),
-                () -> assertEquals(updateNotice.getContent(), "테스트3"),
-                () -> assertEquals(updateNotice.getClassify(),"테스트1")
-        );
-    }
 
     @Test
     @DisplayName("공지사항 삭제 테스트")
@@ -112,11 +104,74 @@ class NoticeRepositoryTest {
 
         System.out.println(result);
 
+    }
+
+    @Autowired
+    HttpServletRequest request;
+
+    @Autowired
+    HttpServletResponse response;
+
+    @Test
+    @DisplayName("공지사항 수정 테스트")
+    public void updateNotice() throws Exception{
+        /*
+        Notice findNotice = noticeRepository.findById(1L).get();
+        NoticeDTO.Notice dto = new NoticeDTO.Notice();
+        dto.setId(1L);
+        dto.setTitle("수정1");
+        dto.setContent("수정2");
+        dto.setClassify("수정3");
+        dto.setAdminId(1L);
+
+        noticeController.noticeUpdate(dto, request, response);
+
+        Notice updateNotice = noticeRepository.findById(1L).get();
+
+        assertAll(
+                () -> assertEquals(updateNotice.getTitle(),"수정"),
+                () -> assertEquals(updateNotice.getContent(), "수정"),
+                () -> assertEquals(updateNotice.getClassify(),"수정")
+        );
+        */
+        Notice findNotice = noticeRepository.findById(1L).get();
+        NoticeDTO.Notice dto = new NoticeDTO.Notice();
+        dto.setId(1L);
+        dto.setTitle("수정1");
+        dto.setContent("수정2");
+        dto.setClassify("수정3");
+        dto.setAdminId(1L);
+
+        Notice updateNotice = noticeService.updateNotice(dto);
+
+        assertAll(
+                () -> assertEquals(updateNotice.getTitle(),"수정"),
+                () -> assertEquals(updateNotice.getContent(), "수정"),
+                () -> assertEquals(updateNotice.getClassify(),"수정")
+        );
+    }
 
 
+    //  삭제 테스트
 
+    @Test
+    @DisplayName("공지사항 삭제 테스트")
+    public void noticeDelete(){
+        Optional<Notice> notice = noticeRepository.findById(1L);
+        Assert.assertTrue(notice.isPresent());
+
+
+        notice.ifPresent(selectNotice ->{
+            noticeRepository.delete(selectNotice);
+        });
+
+        Optional<Notice> deletedNotice = noticeRepository.findById(1L);
+
+        Assert.assertFalse(deletedNotice.isPresent());
 
     }
+
+
 
 }
 
