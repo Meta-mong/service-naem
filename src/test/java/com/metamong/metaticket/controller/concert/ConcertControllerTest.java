ce5c929914bc2c612503a50f795636561e9d97d8
= <=>
2022-06-03 15:10:59 +0900

    feat:controller 수정

--- src/test/java/com/metamong/metaticket/controller/concert/ConcertControllerTest.java
+++ src/test/java/com/metamong/metaticket/controller/concert/ConcertControllerTest.java
@@ -21,47 +21,32 @@ public class ConcertControllerTest {
 
     @Test
     void 공연등록(){
-        Concert concert = concertRepository.findAll().get(0);
-        concert.setId(5L);
-        ConcertDto concertDto = ConcertDto.createDto(concert);
-        concertController.addConcert(concertDto);
+
     }
 
     @Test
     public void 공연상세조회(){
-        Concert concert = concertRepository.findAll().get(0);
-        ConcertDto concertDto = ConcertDto.createDto(concert);
-        ConcertDto concertDto1 =  concertController.concertInfo(concertDto.getId());
-        System.out.println(concertDto1.toString());
+
     }
 
     @Test
     public void 공연수정(){
-        Concert concert = concertRepository.findAll().get(0);
-        ConcertDto concertDto = ConcertDto.createDto(concert);
-        concertDto.setSeatNum(150);
-        concertController.updateConcert(concertDto.getId(), concertDto);
+
     }
 
     @Test
     public void 공연삭제(){
-        concertController.deleteConcert(2L);
+
     }
 
     @Test
     public void 공연조회(){
-        List<Concert> concertList = concertController.concertList();
-        for(Concert c : concertList){
-            System.out.println(c);
-        }
+
     }
 
     @Test
     public void 장르별_공연조회(){
-        List<Concert> concertList = concertController.concertList_Genre(Genre.MUSICAL_DRAMA);
-        for(Concert c : concertList){
-            System.out.println(c);
-        }
+
     }
 
 }
