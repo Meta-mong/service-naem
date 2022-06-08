1b55926e067ccfbccc388030ad60e2984448c4fa
che2rish <whdkfka101@naver.com>
2022-06-08 13:34:21 +0900

    feat: Concert & Files 테스트코드 수정 및 작성

--- src/test/java/com/metamong/metaticket/service/concert/ConcertServiceTest.java
+++ src/test/java/com/metamong/metaticket/service/concert/ConcertServiceTest.java
@@ -2,12 +2,15 @@ package com.metamong.metaticket.service.concert;
 
 import com.metamong.metaticket.domain.concert.Concert;
 import com.metamong.metaticket.domain.concert.Genre;
+import com.metamong.metaticket.domain.concert.Phamplet_File;
 import com.metamong.metaticket.domain.concert.dto.ConcertDto;
 import com.metamong.metaticket.repository.concert.ConcertRepository;
+import com.metamong.metaticket.repository.concert.FilesRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,45 +24,51 @@ public class ConcertServiceTest {
     @Autowired
     ConcertRepository concertRepository;
 
+    @Autowired
+    FilesRepository filesRepository;
+
     @Test
     public void 공연생성(){
         Concert concert = concertRepository.findAll().get(0);
-        concert.setId(1L);
+        concert.setId(2L);
         concertService.addConcert(concert);
     }
 
     @Test
     public void 공연상세조회(){
-        ConcertDto concertDto = concertService.concertInfo(1L);
+        ConcertDto concertDto = concertService.concertInfo(4L);
         System.out.println(concertDto.toString());
     }
 
     @Test
     public void 공연수정(){
-        ConcertDto concertDto = concertService.concertInfo(2L);
+        ConcertDto concertDto = concertService.concertInfo(4L);
         concertDto.setSeatNum(300);
-        concertService.updateConcert(concertDto,2L);
-        System.out.println(concertService.concertInfo(2L).toString());
+        Phamplet_File files = filesRepository.findById(concertDto.getPhamplet()).orElse(null);
+        concertService.updateConcert(concertDto, files);
+        System.out.println(concertService.concertInfo(4L).toString());
     }
 
     @Test
     public void 공연삭제(){
-        concertService.deleteConcert(1L);
+        concertService.deleteConcert(4L);
         assertThat(concertService.concertAllInfo().size()).isEqualTo(1);
     }
 
     @Test
+    @Transactional
     public void 공연조회(){
-        List<Concert> concertList = concertService.concertAllInfo();
-        for(Concert c : concertList){
+        List<ConcertDto> concertList = concertService.concertAllInfo();
+        for(ConcertDto c : concertList){
             System.out.println(c);
         }
     }
 
     @Test
+    @Transactional
     public void 장르별_공연조회(){
-        List<Concert> concertList = concertService.concertGenreInfo(Genre.MUSICAL_DRAMA);
-        for(Concert c : concertList){
+        List<ConcertDto> concertList = concertService.concertGenreInfo(Genre.MUSICAL_DRAMA);
+        for(ConcertDto c : concertList){
             System.out.println(c);
         }
     }
