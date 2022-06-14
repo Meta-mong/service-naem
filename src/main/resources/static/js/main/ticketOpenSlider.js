a4e6d037ba75726b719f8ba244b2152038856042
leesh125 <chelsea12345@naver.com>
2022-06-14 11:45:40 +0900

    thymeleaf dialect layout

---
+++ src/main/resources/static/js/main/ticketOpenSlider.js
@@ -0,0 +1,8 @@
+var mySwiper = new Swiper('.ticket-swiper-container', {
+    slidesPerView: 4,
+    slidesToScroll:4,
+    navigation: {
+      prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+    },
+});
