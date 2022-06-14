3b27b36faf36ee7e336a123d9fa331641f2bb381
khj33398 <khj33398@naver.com>
2022-06-14 14:30:14 +0900

    feat:user related process

--- src/main/java/com/metamong/metaticket/controller/user/SignController.java
+++ src/main/java/com/metamong/metaticket/controller/user/SignController.java
@@ -13,7 +13,9 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.util.HashMap;
--- src/main/java/com/metamong/metaticket/controller/user/UserController.java
+++ src/main/java/com/metamong/metaticket/controller/user/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
