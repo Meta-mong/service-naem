d7a0cf98daae4be99517ede09f8ae675cb573a56
che2rish <whdkfka101@naver.com>
2022-06-17 10:28:14 +0900

    콘서트 상세페이지 연결

--- src/main/resources/static/js/concert/concert.js
+++ src/main/resources/static/js/concert/concert.js
@@ -6,3 +6,9 @@ new Swiper('.concertSwiper  .swiper-container', {
     }, // 자동 재생 여부
    loop: true, // 반복 재생 여부
   });
+
+  // 상세조회 페이지 이동
+  function move_description(id){
+    var id = id;
+    window.location.href = "/concert/Contents/"+id+"/detail";
+  }
