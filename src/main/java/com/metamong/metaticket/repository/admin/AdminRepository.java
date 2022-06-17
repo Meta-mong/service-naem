f69dd34407a70a43847fb308a517c95ca1d7200d
허진솔 <93hjs_@naver.com>
2022-06-17 13:01:15 +0900

    feat:modify

--- src/main/java/com/metamong/metaticket/repository/admin/AdminRepository.java
+++ src/main/java/com/metamong/metaticket/repository/admin/AdminRepository.java
@@ -3,6 +3,8 @@ package com.metamong.metaticket.repository.admin;
 import com.metamong.metaticket.domain.admin.Admin;
 import com.metamong.metaticket.domain.question.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,5 +12,6 @@ import java.util.Optional;
 public interface AdminRepository extends JpaRepository<Admin,Long> {
 
     Admin findByLoginId(String login_id);
+
 
 }
