2377442113fb6837e68cf2c380cbc2a9f4c48dcd
khj33398 <khj33398@naver.com>
2022-06-17 11:43:14 +0900

    feat:ticketopen page linking done

--- src/main/resources/static/js/ticketopen/ticket_open.js
+++ src/main/resources/static/js/ticketopen/ticket_open.js
@@ -3,3 +3,9 @@ window.addEventListener("load", function (){
        location.href="/ticketopen/"+$("#genre").val();
     });
 });
+
+function genreCheck(data){
+    if(data != ""){
+        $("#genre").val(data).prop("selected", true);
+    }
+}
