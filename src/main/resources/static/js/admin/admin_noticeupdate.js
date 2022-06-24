0ef2aa8c21c2507b6f35bc8680be787f13820f72
che2rish <whdkfka101@naver.com>
2022-06-24 17:22:59 +0900

    css 수정 및 장르, 관람등급 수정 등

--- src/main/resources/static/js/admin/admin_noticeupdate.js
+++ src/main/resources/static/js/admin/admin_noticeupdate.js
@@ -1,24 +1,21 @@
-window.addEventListener("load", function() {
-    $("#noticeupdate").on("click", function(){
-        noticeId = $("#noticeId").text();
-//        alert(noticeId);
-        $.ajax({
-            url : "/admin/noticeupdate/"+noticeId,
-            type : "POST",
-            data : {"title" : $("#title").val(), "content" : $("#content").val(), "classify" : $("#classify").val()},
-            dataType : "json",
-            success : function (data){
-               if(data.result==true){
-                    alert("공지사항 수정 완료");
-                    location.replace("/admin/anlist");
-               }else{
-                    alert("공지사항 수정 실패")
-               }
-            },
-            error : function (data){
-               alert("공지사항 수정 실패. 다시 시도해주세요.");
-            }
-       })
-    });
-
- });
+ function noticeupdate(id){
+         var noticeId = id;
+ //        alert(noticeId);
+         $.ajax({
+             url : "/admin/noticeupdate/"+noticeId,
+             type : "POST",
+             data : {"title" : $("#title").val(), "content" : $("#content").val(), "classify" : $("#classify").val()},
+             dataType : "json",
+             success : function (data){
+                if(data.result==true){
+                     alert("공지사항 수정 완료");
+                     location.replace("/admin/anlist");
+                }else{
+                     alert("공지사항 수정 실패")
+                }
+             },
+             error : function (data){
+                alert("공지사항 수정 실패. 다시 시도해주세요.");
+             }
+        })
+ }
